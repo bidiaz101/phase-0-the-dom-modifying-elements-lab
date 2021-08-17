08d56e3f0aa7dc7175d54bb7d8ce769707f40975
Brian Diaz <bidiaz101@gmail.com>
2021-08-16 20:56:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const main = document.getElementById('main');
 main.remove();
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-document.body.appendChild(newHeader);
-newHeader.innerHTML = "Brian is the champion";
+newHeader.textContent = "Brian is the champion.";
+document.body.append(newHeader);
